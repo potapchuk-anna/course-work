61d1eb3f9da5a8534ed98d54cb4f73a08dcd575a
Анна Потапчук <potapchuk@dlit.dp.ua>
2021-12-20 13:27:13 +0200

    Replication modified

--- CourseWork/MainWindow.xaml.cs
+++ CourseWork/MainWindow.xaml.cs
@@ -26,15 +26,26 @@ namespace CourseWork
         public MainWindow()
         {
             InitializeComponent();
-            EducationalSystemContext context = EducationalSystemContext.Instance;           
-            context.Classes.Load();       
-            context.Teachers.Load();            
-            context.Subjects.Load();                
-            context.Students.Load();
-            context.Works.Load();
-            context.Grades.Load();
-            EducationalSystemContext context1 = new EducationalSystemContext("Host=localhost;Port=5432;Database=new_educational_system;Username=user;Password=12345;CommandTimeout=1000;Timeout=1000");
-            context1.CreateSubscription();
+            EducationalSystemContext main = EducationalSystemContext.Instance;
+            EducationalSystemContext replica = new EducationalSystemContext("Host=localhost;Port=5432;Database=new_educational_system;Username=user;Password=12345;CommandTimeout=1000;Timeout=1000");
+            replica.CreateSubscription();
+            if(!(main.Classes.Any()&& main.Teachers.Any()&& main.Subjects.Any()&& main.Students.Any()
+                && main.Works.Any()&&main.Grades.Any()))
+            {
+                main.Classes.AddRange(replica.Classes);
+                main.Teachers.AddRange(replica.Teachers);
+                main.Subjects.AddRange(replica.Subjects);
+                main.Students.AddRange(replica.Students);
+                main.Works.AddRange(replica.Works);
+                main.Grades.AddRange(replica.Grades);
+            }
+            main.Classes.Load();       
+            main.Teachers.Load();            
+            main.Subjects.Load();                
+            main.Students.Load();
+            main.Works.Load();
+            main.Grades.Load();
+           
         }        
     }
 }
